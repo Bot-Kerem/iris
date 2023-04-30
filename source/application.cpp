26d5f94e056c2c198f1b6213683976d83415b93a
Bot-Kerem <akerem1434@gmail.com>
2023-05-01 00:02:06 +0300

    Updated Application class

--- source/application.cpp
+++ source/application.cpp
@@ -2,9 +2,12 @@
 #include <spdlog/spdlog.h>
 
 #include <algorithm>
+#include <glm/ext/matrix_clip_space.hpp>
 #include <utility>
 
 #include "iris/application.hpp"
+
+glm::mat4 view_matrix;
 
 void Application::main_loop() {
   while (!window.close_requested()) {
@@ -26,6 +29,9 @@ void Application::build_app() {
   spdlog::info("Loading OpenGL");
   gladLoadGLLoader(reinterpret_cast<GLADloadproc>(Window::get_proc_address));
 
+  /* Set view matrix */
+  view_matrix = glm::ortho(0.0f, static_cast<float>(800), 0.0f, static_cast<float>(600));
+
   auto [scene, scene_name] = build();
   scenes.insert(std::make_pair(scene_name, std::move(scene)));
 
@@ -45,4 +51,8 @@ int Application::exec() {
   spdlog::info("Entering the main loop");
   main_loop();
   return 0;
+}
+
+void framebuffer_size_callback(const size_t w, const size_t h) {
+  view_matrix = glm::ortho(0.0f, static_cast<float>(w), 0.0f, static_cast<float>(h));
 }
