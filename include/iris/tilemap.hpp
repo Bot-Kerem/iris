d5b0ed8e60d777d1a2add10d1741545ebb84c82c
Bot-Kerem <akerem1434@gmail.com>
2023-05-12 15:04:44 +0300

    Updated Tilemap

--- include/iris/tilemap.hpp
+++ include/iris/tilemap.hpp
@@ -1,19 +1,30 @@
 #pragma once
 
+#include <cstddef>
+#include <map>
 #include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
 #include "mesh.hpp"
 #include "shader.hpp"
 #include "texture.hpp"
+#include "tile.hpp"
 
 constexpr float tile_size = 100.0f;
 
 class Tilemap : public Mesh {
  private:
-  Texture tex;
-
   std::shared_ptr<Shader> tile_shader;
+  std::map<std::string, std::unique_ptr<Texture>> textures;
+  std::vector<std::vector<std::unique_ptr<Tile>>> tiles;
+
  public:
   Tilemap(std::shared_ptr<Shader> shader) noexcept;
+
+  void set_tile(std::unique_ptr<Tile>& tile,
+                std::pair<size_t, size_t> position);
 
   void draw() override;
 };
--- source/tilemap.cpp
+++ source/tilemap.cpp
@@ -7,6 +7,8 @@
 #include <glm/ext/vector_float3.hpp>
 #include <glm/ext/vector_int2_sized.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <memory>
+#include <vector>
 
 #include "iris/texture.hpp"
 
@@ -35,20 +37,35 @@ Tilemap::Tilemap(std::shared_ptr<Shader> shader) noexcept
   m_vertex_array.vertex_attrib_pointer(1, 2, sizeof(TileVertex),
                                        offsetof(TileVertex, uv));
 
-  tex.load_from_file("res/luffy.jpg", Texture::Format::RGB);
+  std::shared_ptr<Texture> textures[] = {
+  std::make_unique<Texture>(),
+  std::make_unique<Texture>(),
+  std::make_unique<Texture>(),
+  };
+
+  textures[0]->load_from_file("res/luffy.jpg", Texture::Format::RGB);
+  textures[1]->load_from_file("res/nami.jpg", Texture::Format::RGB);
+  textures[2]->load_from_file("res/edward.jpg", Texture::Format::RGB);
+
+  const glm::i32vec2 tile_map_size{4, 3};
+  for (int y = 0; y < tile_map_size.y; y++) {
+    tiles.push_back({});
+    for (int x = 0; x < tile_map_size.x; x++) {
+      auto tile = std::make_unique<Tile>(textures[y]);
+      tiles[y].push_back(std::move(tile));
+    } 
+  }
 }
 
 void Tilemap::draw() {
-  const glm::i32vec2 tile_map_size{4, 3};
-
-  tex.bind(0);
   tile_shader->use();
-  for (int y = 0; y < tile_map_size.y; y++) {
-    for (int x = 0; x < tile_map_size.x; x++) {
+  for (size_t y = 0; y < tiles.size(); y++) {
+    for (size_t x = 0; x < tiles[y].size(); x++) {
       tile_shader->set_mat4(
           "model", glm::translate(glm::mat4(1.0f),
                                   glm::vec3(400.0f + (x * tile_size),
                                             300.0f + (y * tile_size), 0.0)));
+      tiles[y][x]->texture->bind(0);
       draw_arrays(6, PrimitiveType::Triangle);
     }
   }
